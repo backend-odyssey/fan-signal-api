b16163cd0a4fb1bf4e4d8dce70533c73c5585c89
deeev-sb <sb2504@naver.com>
2024-12-21 19:39:14 +0900

    🗃Update Redis Configuration

--- src/main/kotlin/backend_incubator/fan_signal_api/config/RedisConfig.kt
+++ src/main/kotlin/backend_incubator/fan_signal_api/config/RedisConfig.kt
@@ -1,20 +1,25 @@
 package backend_incubator.fan_signal_api.config
 
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.context.annotation.Primary
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory
 import org.springframework.data.redis.connection.RedisPassword
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
-import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.core.ReactiveRedisTemplate
+import org.springframework.data.redis.core.ReactiveStringRedisTemplate
+import org.springframework.data.redis.serializer.RedisSerializationContext
 import org.springframework.data.redis.serializer.StringRedisSerializer
 
 @Configuration
 class RedisConfig(private val redisProperties: RedisProperties) {
 
     @Bean
-    fun redisConnectionFactory(): RedisConnectionFactory {
+    @Primary
+    fun reactiveRedisConnectionFactory(): ReactiveRedisConnectionFactory {
         val config = RedisStandaloneConfiguration()
         config.hostName = redisProperties.host
         config.port = redisProperties.port
@@ -25,11 +30,21 @@ class RedisConfig(private val redisProperties: RedisProperties) {
     }
 
     @Bean
-    fun redisTemplate(): RedisTemplate<String, Any> {
-        val template = RedisTemplate<String, Any>()
-        template.connectionFactory = redisConnectionFactory()
-        template.keySerializer = StringRedisSerializer()
-        template.valueSerializer = StringRedisSerializer()
-        return template
+    @Qualifier("reactiveStringRedisTemplate")
+    fun reactiveStringRedisTemplate(factory: ReactiveRedisConnectionFactory): ReactiveStringRedisTemplate {
+        return ReactiveStringRedisTemplate(factory)
+    }
+
+    @Bean
+    @Qualifier("reactiveRedisTemplate")
+    fun reactiveRedisTemplate(factory: ReactiveRedisConnectionFactory): ReactiveRedisTemplate<String, String> {
+        val keySerializer = StringRedisSerializer()
+        val valueSerializer = StringRedisSerializer()
+
+        val serializationContext = RedisSerializationContext.newSerializationContext<String, String>(keySerializer)
+            .value(valueSerializer)
+            .build()
+
+        return ReactiveRedisTemplate(factory, serializationContext)
     }
 }
